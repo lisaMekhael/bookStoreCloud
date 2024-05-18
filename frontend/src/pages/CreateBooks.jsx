699b10000e05bf3c304f702dde820d83209da277
lisaMekhael <121225179+lisaMekhael@users.noreply.github.com>
2024-05-18 06:25:08 +0300

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -26,7 +26,7 @@ const CreateBooks = () => {
 
     setLoading(true);
     axios
-      .post("http://44.215.104.196/books", formData, {
+      .post("http://54.167.90.253:5173/books", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
