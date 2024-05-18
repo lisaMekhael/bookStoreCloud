35b402c781b073a68be3a630524bac114c8c3b10
lisaMekhael <121225179+lisaMekhael@users.noreply.github.com>
2024-05-18 06:28:07 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://44.215.104.196/books")
+      .get("http://54.167.90.253:5173/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
