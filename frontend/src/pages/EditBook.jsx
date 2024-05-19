346473380d72ebb93e632be6b16a02560d324cf2
Lisa Samir Hana <121225179+lisaMekhael@users.noreply.github.com>
2024-05-19 16:36:40 +0300

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -19,7 +19,7 @@ const EditBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://54.167.90.253:5173/books/${id}`)
+      .get(`http://44.200.214.117:5173/books/${id}`)
       .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear);
