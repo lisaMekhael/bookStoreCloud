a6a4bc31c42d8618fedec41fdc7ebe9075f98fda
Lisa Samir Hana <121225179+lisaMekhael@users.noreply.github.com>
2024-05-19 16:37:05 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://54.167.90.253:5173/books")
+      .get("http://44.200.214.117:5173/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
