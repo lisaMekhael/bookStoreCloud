e7c59a807bdf858623b4a3eec63316e4454f9f25
Lisa Samir Hana <121225179+lisaMekhael@users.noreply.github.com>
2024-05-21 02:47:25 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// vite.config.js
 export default defineConfig({
   plugins: [react()],
 })
